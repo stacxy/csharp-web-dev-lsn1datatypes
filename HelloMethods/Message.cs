cd179692175934f9a037e44121cd9a06c7fb512d
stacey <staceystroh.84@gmail.com>
2020-12-06 21:25:12 -0600

    initial commit

--- HelloMethods/Message.cs
+++ HelloMethods/Message.cs
@@ -12,6 +12,10 @@
             {
                 return "Bonjour le monde";
             }
+            else if (lang.Equals("de")) 
+            {
+                return "Hallo Welt";
+            }
             else
             {
                 return "Hello World";
